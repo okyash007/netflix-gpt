b9c2c8dfcdbab9dbb2a5b60f5680f98d54f2ddc0
Yash Verma <helloyashverma@gmail.com>
2023-09-12 18:01:21 +0530

    SignIn/SignUp && Header, Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Body from './components/Body'
 
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold text-green-600' >hello</h1>
-    </>
+    <Body/>
   );
 }
 
