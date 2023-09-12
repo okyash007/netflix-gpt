f98aa33ca108ddd8ff8928c9bc9fbbffe994e4e7
Yash Verma <helloyashverma@gmail.com>
2023-09-12 20:10:29 +0530

    SignIn/SignUp validation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { validateUser } from "../utils/validation";
 
 const Header = () => {
   const [signIn, setsignIn] = useState(true);
+  const [errorMessage,seterrorMessage] = useState(null)
+
+  const email = useRef(null)
+  const password = useRef(null)
 
   const togglesignIn = () => {
     setsignIn(!signIn);
   };
 
+  const clicked = () => {
+    const message = validateUser( email.current.value,password.current.value )
+    seterrorMessage(message)
+  };
+
   return (
     <>
-      <div className="w-full h-96 max-sm:h-96 absolute z-10 bg-gradient-to-b from-black max-sm:from-red-950">
+      <div className="w-full h-96 max-sm:h- absolute z-10 bg-gradient-to-b from-black max-sm:from-red-950">
         <img
           className="w-48 absolute z-20 m-3"
           src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
           alt="logo"
         />
       </div>
-      <form className="flex flex-col p-14 bg-black absolute z-40 m-auto top-0 bottom-0 right-0 left-0 h-3/4 w-96 text-white rounded bg-opacity-80 max-sm:w-full max-sm:h-full max-sm:z-0 max-sm:py-28">
+      <form onSubmit={(e)=> e.preventDefault() } className="flex flex-col p-14 bg-black absolute z-40 m-auto top-0 bottom-0 right-0 left-0 h-3/4 w-96 text-white rounded bg-opacity-80 max-sm:w-full max-sm:h-full max-sm:z-0 max-sm:py-28">
         <h1 className="font-sans font-semibold text-4xl my-4">
           {signIn ? "Sign In" : "Sign Up"}
         </h1>
@@ -28,18 +38,25 @@ const Header = () => {
           />
         )}
         <input
+        ref={email}
           type="text"
           placeholder="Email"
           className="p-3 my-2 text-sm bg-gray-700 rounded z-50"
         />
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="p-3 my-2 text-sm bg-gray-700 rounded"
         />
-        <button className="bg-red-600 p-3 my-4 text-lg rounded">
+        <p className="text-xs font-semibold text-red-600" >{errorMessage}</p>
+        <button
+          className="bg-red-600 p-3 my-4 text-lg rounded"
+          onClick={clicked}
+        >
           {signIn ? "Sign In" : "Sign Up"}
         </button>
+        
         <div className="text-xs font-semibold text-gray-400 py-6 cu">
           {signIn ? "New to Netflix?" : "Alredy a user?"}{" "}
           <p
